3a398dde63ed7df9fa16814ceefe7bc00e97b39e
JaimeApBa <38909325+JaimeApBa@users.noreply.github.com>
2023-01-04 16:09:29 +0100

    Update config.ts

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,7 +1,7 @@
 import { HttpHeaders } from '@angular/common/http';
 
 // tslint:disable-next-line:eofline
-export const URL_SERVICES = 'https://api-propietarispuntcat.onrender.com';
+export const URL_SERVICES = 'https://api-propietarispuntcat.onrender.com/api';
 
 // URL Google Geocoding API
 export const URL_GEOCODING = 'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDERh-kPwhS3ZBo359IkyVTr4y9meso5KM&language=ca&components=country:ES&address=';
